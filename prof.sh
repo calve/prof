3f818cb3f09039f9e2786f2287cceffab8ed0cad
calve <calvinh34@gmail.com>
2013-10-15 01:29:01 +0200

    Better solution for password, and know use awk to parse projects

--- prof.sh
+++ prof.sh
@@ -1,30 +1,36 @@
 ## Script pour envoyé un fichier sur prof
 # CC0 No rights reserved
 
-login=login
-passwd=password #oui, en clair, ca craint
+baseurl="https://prof.fil.univ-lille1.fr/"
+login=debusschere
 
+#On récupère le mot de passe
+echo -n Password: 
+read -s -r passwd
+echo
 
 #On initialize la session
-curl -ss "https://prof.fil.univ-lille1.fr/index.php"  -c cookie > tmp
-curl -ss "https://prof.fil.univ-lille1.fr/login.php" --data "login=$login&passwd=$passwd&++O+K++=Valider" -b cookie > tmp
+curl -ss $baseurl"index.php"  -c cookie > tmp
 
-# On récupere la liste des ids
-curl -ss "https://prof.fil.univ-lille1.fr/select_projet.php" -b cookie | grep /OPTION
+curl -ss $baseurl"login.php" --data "login=$login" --data "passwd=$passwd" --data "++O+K++=Valider" -b cookie > tmp
+
+# On récupere la liste des ids 
+                                                #awk -F"VALUE=\"|\">|</OPTION>" '{ print $3 " -> " $2}'
+curl -ss $baseurl"select_projet.php" -b cookie | grep "<\/OPTION>" | awk -F"VALUE=\\\"|\">|</OPTION>" '{ print $3 " -> " $2}'
 read -p "Please select an option value above : " projectvalue
 
 #On récupere la liste et l'état des TPS
-curl -ss "https://prof.fil.univ-lille1.fr/main.php" --data "id_projet=$projectvalue" -b cookie | grep "echeance"
+curl -ss $baseurl"main.php" --data "id_projet=$projectvalue" -b cookie | grep "echeance"
 read -p "Please select an id above : " idtp
 
-curl -ss "https://prof.fil.univ-lille1.fr/upload.php" --data "id=$idtp" -b cookie > tmp
+curl -ss $baseurl"upload.php" --data "id=$idtp" -b cookie > tmp
 
 #on upload le fichier
 if [ "$1" ]; then
-    curl -ss "https://prof.fil.univ-lille1.fr/upload2.php" --form "fichier1=@$1" --form "MAX_FILE_SIZE=1000000" -b cookie > tmp
+    curl -ss $baseurl"upload2.php" --form "fichier1=@$1" --form "MAX_FILE_SIZE=1000000" -b cookie > tmp
 else
     read -p "Give me the file : " file
-    curl -ss "https://prof.fil.univ-lille1.fr/upload2.php" --form "fichier1=@$file" --form "MAX_FILE_SIZE=1000000" -b cookie > tmp
+    curl -ss $baseurl"upload2.php" --form "fichier1=@$file" --form "MAX_FILE_SIZE=1000000" -b cookie > tmp
 fi
 
 
