b79593827ec16ca9a6e58b4913d483a4edad3ded
calve <calvinh34@gmail.com>
2014-10-26 23:13:51 +0100

    Exit gracefully if no connection available

--- prof/init.py
+++ prof/init.py
@@ -6,13 +6,23 @@ prof_session = requests.Session()
 
 
 def initiate_session(login, password):
-    """Initiate a session globally used in prof. Log and retrieve the cookie"""
-    prof_session.get(baseurl+"/index.php")
-    payload = {
-        'login': login,
-        'passwd': urllib.parse.quote_plus(password),
-        '++O+K++': 'Valider'
-    }
-    work_html = prof_session.post(baseurl+"/login.php", params=payload)
+    """
+    Initiate a session globally used in prof :
+      + Retrive the cookie
+      + Log to prof
 
-    return work_html
+    Returns a buffer containing html of the after login page
+    """
+    try:
+        prof_session.get(baseurl+"/index.php")
+        payload = {
+            'login': login,
+            'passwd': urllib.parse.quote_plus(password),
+            '++O+K++': 'Valider'
+        }
+        work_html = prof_session.post(baseurl+"/login.php", params=payload)
+
+        return work_html
+    except:
+        print("{baseurl} not reachable. Verify your connection".format(baseurl=baseurl))
+        exit(1)
