0fb1e3ae4e8702b227537cf7ec06483085de184d
calve <calvinh34@gmail.com>
2015-03-03 10:31:27 +0100

    First step to package to Pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
+
 setup(
     name='prof',
     packages=['prof'],
     version='1.0',
-    description='A tool to upload student work',
+    description='A tool to upload student work to http://prof.fil.univ-lille1.fr',
     author='calve',
     author_email='calvinh34@gmail.com',
     url='http://github.com/calve/prof',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+    ],
+    install_requires=[
+        "requests",
+    ]
 )
