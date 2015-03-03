8b94f8ce860d9ec43c9a7a5a871a3f38aac8fd91
calve <calvinh34@gmail.com>
2015-03-03 11:49:30 +0100

    Version ``1.0`` has already been used on Pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prof',
-    version='1.0',
+    version='1.0.0',
     packages=find_packages(),
     description='A tool to upload student work to http://prof.fil.univ-lille1.fr',
     author='calve',
